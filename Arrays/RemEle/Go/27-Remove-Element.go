d6c3506a8cb034b77a3f035e5df71300ada02723
Santosh Kowshik <santosh.kowshik20@gmail.com>
2025-02-17 23:25:53 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 18.65%] - LeetPush

---
+++ Arrays/RemEle/Go/27-Remove-Element.go
@@ -0,0 +1,11 @@
+func removeElement(nums []int, val int) int {
+    k := 0 
+
+    for i := range nums{
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k += 1
+        }
+    }
+    return k    
+}

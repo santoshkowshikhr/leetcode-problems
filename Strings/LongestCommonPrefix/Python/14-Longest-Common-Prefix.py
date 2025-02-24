6212c98ed9f4d41301d5914e2c38298a28ee53e9
Santosh Kowshik <santosh.kowshik20@gmail.com>
2025-02-24 21:46:19 +0530

    Update 14-Longest-Common-Prefix.py

--- Strings/LongestCommonPrefix/Python/14-Longest-Common-Prefix.py
+++ Strings/LongestCommonPrefix/Python/14-Longest-Common-Prefix.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
-            return \\
+            return ""
         
         for i in range(len(strs[0])):
             char = strs[0][i]

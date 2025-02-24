15684a87d931ad72a3b75f3b88089b03e5afcf35
Santosh Kowshik <santosh.kowshik20@gmail.com>
2025-02-24 21:41:22 +0530

    [14] [Time Beats: 100.00%] [Memory Beats: 73.78%] - LeetPush

---
+++ Strings/LongestCommonPrefix/Go/14-Longest-Common-Prefix.go
@@ -0,0 +1,16 @@
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0 {
+        return \\
+    }
+
+    for i := 0; i < len(strs[0]); i++ {
+        char := strs[0][i]
+
+        for j := 1; j < len(strs); j++ {
+            if i >= len(strs[j]) || strs[j][i] != char {
+                return strs[0][:i]
+            }
+        }
+    }
+    return strs[0]
+}
